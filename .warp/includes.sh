58c9ae426d7dd00745e276035be3c7c65288873d
Sebastian Arrubia <sarrubia@Sebastians-MacBook-Pro.local>
2018-10-06 10:19:13 -0300

    Webserver setup

--- .warp/includes.sh
+++ .warp/includes.sh
@@ -3,12 +3,12 @@
 # INCLUDE WARP FRAMEWORK
 . "$PROJECTPATH/.warp/lib/version.sh"
 . "$PROJECTPATH/.warp/lib/commit.sh"
-. "$PROJECTPATH/.warp/lib/banner.sh"
 . "$PROJECTPATH/.warp/lib/env.sh"
 . "$PROJECTPATH/.warp/lib/message.sh"
 . "$PROJECTPATH/.warp/lib/net.sh"
 . "$PROJECTPATH/.warp/lib/question.sh"
 . "$PROJECTPATH/.warp/lib/check.sh"
+. "$PROJECTPATH/.warp/lib/banner.sh"
 
 # INCLUDE COMMANDS
 . "$PROJECTPATH/.warp/bin/mysql.sh"
