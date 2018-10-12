46d69fb1d9fbd53eead7d25af5723add6651ddd9
Matias Anoniz <matiasanoniz@gmail.com>
2018-10-12 17:08:33 -0300

    improvements in console messages for mac and linux

--- .warp/lib/message.sh
+++ .warp/lib/message.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# ANSI color codes
+RS="\e[\033[0m"    # reset
+HC="\e[\033[1m"    # hicolor
+UL="\e[\033[4m"    # underline
+INV="\e[\033[7m"   # inverse background and foreground
+FBLK="\e[\033[30m" # foreground black
+FRED="\e[\033[31m" # foreground red
+FGRN="\e[\033[32m" # foreground green
+FYEL="\e[\033[33m" # foreground yellow
+FBLE="\e[\033[34m" # foreground blue
+FMAG="\e[\033[35m" # foreground magenta
+FCYN="\e[\033[36m" # foreground cyan
+FWHT="\e[\033[37m" # foreground white
+FGRY="\e[\033[90m" # foreground gray
+BBLK="\e[\033[40m" # background black
+BRED="\e[\033[41m" # background red
+BGRN="\e[\033[42m" # background green
+BYEL="\e[\033[43m" # background yellow
+BBLE="\e[\033[44m" # background blue
+BMAG="\e[\033[45m" # background magenta
+BCYN="\e[\033[46m" # background cyan
+BWHT="\e[\033[47m" # background white
+
 function warp_message()
 {
     printf "$1\n"
@@ -7,32 +30,32 @@ function warp_message()
 
 function warp_message_error()
 {
-    warp_message "\e[91m$1\e[0m"
+    warp_message "$FRED $1 $RS"
 }
 
 function warp_message_ok()
 {
-    warp_message "\e[92m$1\e[0m"
+    warp_message "$FGRN $1 $RS"
 }
 
 function warp_message_info()
 {
-    warp_message "\e[96m$1\e[0m"
+    warp_message "$FCYN $1 $RS"
 }
 
 function warp_message_info2()
 {
-    warp_message "\e[90m$1\e[0m"
+    warp_message "$FGRY $1 $RS"
 }
 
 function warp_message_warn()
 {
-    warp_message "\e[93m$1\e[0m"
+    warp_message "$FYEL $1 $RS"
 }
 
 function warp_message_bold()
 {
-    warp_message "\e[1m$1\e[21m"
+    warp_message "$HC $1 $RC"
 }
 
 
