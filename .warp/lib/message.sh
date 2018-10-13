bad4838f886a0a6ceef24cc41e6a8593d3f64cb7
Matias Anoniz <matiasanoniz@gmail.com>
2018-10-12 21:28:39 -0300

    fix message color linux, mac

--- .warp/lib/message.sh
+++ .warp/lib/message.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # ANSI color codes
-RS="\e[\033[0m"    # reset
-HC="\e[\033[1m"    # hicolor
-UL="\e[\033[4m"    # underline
-INV="\e[\033[7m"   # inverse background and foreground
-FBLK="\e[\033[30m" # foreground black
-FRED="\e[\033[31m" # foreground red
-FGRN="\e[\033[32m" # foreground green
-FYEL="\e[\033[33m" # foreground yellow
-FBLE="\e[\033[34m" # foreground blue
-FMAG="\e[\033[35m" # foreground magenta
-FCYN="\e[\033[36m" # foreground cyan
-FWHT="\e[\033[37m" # foreground white
-FGRY="\e[\033[90m" # foreground gray
-BBLK="\e[\033[40m" # background black
-BRED="\e[\033[41m" # background red
-BGRN="\e[\033[42m" # background green
-BYEL="\e[\033[43m" # background yellow
-BBLE="\e[\033[44m" # background blue
-BMAG="\e[\033[45m" # background magenta
-BCYN="\e[\033[46m" # background cyan
-BWHT="\e[\033[47m" # background white
+RS="\e[0m"    # reset
+HC="\e[1m"    # hicolor
+UL="\e[4m"    # underline
+INV="\e[7m"   # inverse background and foreground
+FBLK="\e[30m" # foreground black
+FRED="\e[31m" # foreground red
+FGRN="\e[32m" # foreground green
+FYEL="\e[33m" # foreground yellow
+FBLE="\e[34m" # foreground blue
+FMAG="\e[35m" # foreground magenta
+FCYN="\e[36m" # foreground cyan
+FWHT="\e[37m" # foreground white
+FGRY="\e[90m" # foreground gray
+BBLK="\e[40m" # background black
+BRED="\e[41m" # background red
+BGRN="\e[42m" # background green
+BYEL="\e[43m" # background yellow
+BBLE="\e[44m" # background blue
+BMAG="\e[45m" # background magenta
+BCYN="\e[46m" # background cyan
+BWHT="\e[47m" # background white
 
 function warp_message()
 {
