d63fbb2ea4c5f56e4596bbaeb3cbf7a3ff5dcf37
Matias Anoniz <matiasanoniz@gmail.com>
2018-10-30 17:40:17 -0300

    added stop command

---
+++ .warp/bin/stop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#######################################
+# Stop the server
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+
+##-c ####################-c #################
+stop() {
+
+  if [ $(isRunning) = true ]; then
+    # stop all docker containers
+    docker-compose -f $DOCKERCOMPOSEFILE down
+  fi
+
+}
+
+#######################################
+# Check if the docker-components are running
+# Globals:
+#   DOCKERCOMPOSEFILE
+# Arguments:
+#   None
+# Returns:
+#   true|false
+#######################################
+isRunning() {
+  dockerStatusOutput=$(docker-compose -f $DOCKERCOMPOSEFILE ps -q | xargs docker inspect --format='{{ .State.Status }}' | sed 's:^/::g' | grep -i running)
+  outputSize=${#dockerStatusOutput}
+  if [ "$outputSize" -gt 0 ]; then
+    echo true
+  else
+    echo false
+  fi
+}
