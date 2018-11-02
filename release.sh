b60df1d966a2dc004e8b42f2f9c42300d4d79956
Sebastian Arrubia <sarrubia@summasolutions.net>
2018-11-02 18:24:58 -0300

    adding commit version and download landing page

--- release.sh
+++ release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+COMMIT_VERSION=`git rev-parse --short HEAD`
+BUILD_VERSION=`tail -n 1 .warp/lib/version.sh | sed "s/WARP_VERSION=//g" | tr -d '"'`
+TAG_VERSIONS=`git tag -l | sort -r`
+
+
+cat release/commit.sh.template | sed -e "s/COMMIT_VERSION/${COMMIT_VERSION}/" > .warp/lib/commit.sh
+
 tar cJf warparchive.tar.xz --exclude=".DS_Store" .warp/.
 cat warp > dist/warp
 cat warparchive.tar.xz >> dist/warp
