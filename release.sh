f5caac3682da9e8c69c88e640b12f43731014a83
Sebastian Arrubia <sarrubia@summasolutions.net>
2018-11-05 10:10:03 -0300

    Adding release version automatically

--- release.sh
+++ release.sh
@@ -2,11 +2,14 @@
 
 
 COMMIT_VERSION=`git rev-parse --short HEAD`
-BUILD_VERSION=`tail -n 1 .warp/lib/version.sh | sed "s/WARP_VERSION=//g" | tr -d '"'`
+#BUILD_VERSION=`tail -n 1 .warp/lib/version.sh | sed "s/WARP_VERSION=//g" | tr -d '"'`
+BUILD_VERSION=`date +%Y.%m.%d`
 TAG_VERSIONS=`git tag -l | sort -r`
 
 
 cat release/commit.sh.template | sed -e "s/COMMIT_VERSION/${COMMIT_VERSION}/" > .warp/lib/commit.sh
+cat release/version.sh.template | sed -e "s/BUILD_VERSION/${BUILD_VERSION}/" > .warp/lib/version.sh
+
 
 tar cJf warparchive.tar.xz --exclude=".DS_Store" .warp/.
 cat warp > dist/warp
