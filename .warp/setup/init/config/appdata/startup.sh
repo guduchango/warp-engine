6f6509b3fe62be1529ad466dd857e476c95e07c6
Matias <matiasanoniz@gmail.com>
2018-11-09 19:51:12 -0300

    fix permissions in script appdata for magento 2

--- .warp/setup/init/config/appdata/startup.sh
+++ .warp/setup/init/config/appdata/startup.sh
@@ -2,19 +2,23 @@
 #
 # Set the correct file permissions for Magento 2
 
+PATH_ROOT_SITE="/var/www/html"
+
 while true;
 do
-	chgrp 33 /var/www/html
+	# Change group to 33 (www-data inside the container)
+	chgrp 33 $PATH_ROOT_SITE
+	chgrp 33 $PATH_ROOT_SITE/bin/magento
 
-	for D in $(ls /var/www/html); do
-        chgrp -R 33 "${D}"
+	for D in $(ls $PATH_ROOT_SITE); do
+        chgrp -R 33 "$PATH_ROOT_SITE/${D}"
 
-        find "${D}" -type d -exec chmod ug+rwx {} \; # Make folders traversable and read/write
-		find "${D}" -type f -exec chmod ug+rw {} \;  # Make files read/write 
+        find "$PATH_ROOT_SITE/${D}" -type d -exec chmod ug+rwx {} \; # Make folders traversable and read/write
+		find "$PATH_ROOT_SITE/${D}" -type f -exec chmod ug+rw {} \;  # Make files read/write 
 	done
 
-	chmod 775 /var/www/html/warp
-	chmod 775 /var/www/html/bin/magento
+	chmod 775 $PATH_ROOT_SITE/warp
+	chmod 775 $PATH_ROOT_SITE/bin/magento
 
   sleep 1800 # 30 minutes
 done
