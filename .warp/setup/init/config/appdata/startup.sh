a7717594e1f9895d0cc1e057d262b133ce28c090
Matias <matiasanoniz@gmail.com>
2018-11-09 18:25:53 -0300

    fix permissions script appdata

--- .warp/setup/init/config/appdata/startup.sh
+++ .warp/setup/init/config/appdata/startup.sh
@@ -4,16 +4,17 @@
 
 while true;
 do
-	for D in /var/www/html/*; do
-	    if [ -d "${D}" ]; then
-	        chgrp -R 33 "${D}"
+	chgrp 33 /var/www/html
 
-	        find "${D}" -type d -exec chmod ug+rwx {} \; # Make folders traversable and read/write
-			find "${D}" -type f -exec chmod ug+rw {} \;  # Make files read/write 
-		elif [ -f "${D}" ]; then
-			chmod ug+rw "${D}"
-	    fi
+	for D in $(ls /var/www/html); do
+        chgrp -R 33 "${D}"
+
+        find "${D}" -type d -exec chmod ug+rwx {} \; # Make folders traversable and read/write
+		find "${D}" -type f -exec chmod ug+rw {} \;  # Make files read/write 
 	done
+
+	chmod 775 /var/www/html/warp
+	chmod 775 /var/www/html/bin/magento
 
   sleep 1800 # 30 minutes
 done
