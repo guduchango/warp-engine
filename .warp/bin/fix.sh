73f8a306e74b5f4126360b660cd3f1e46072f1b2
Matias Anoniz <matiasanoniz@gmail.com>
2019-04-04 17:37:06 -0300

    Add commands for each service

--- .warp/bin/fix.sh
+++ .warp/bin/fix.sh
@@ -25,28 +25,56 @@ function fix_add_user()
     exit 1;
 }
 
-function fix_permissions() 
+function fix_php()
 {
 
-    if [ "$1" = "-h" ] || [ "$1" = "--help" ]
-    then
-        fix_help_usage 
-        exit 1
-    fi;
+    warp_message "* Applying permissions for php logs .warp/docker/volumes/php-fpm $(warp_message_ok [ok])"
 
-    if [ "$1" = "--addUser" ]
-    then
-        fix_add_user 
-        exit 1
-    fi;
+    # add permission php logs
 
-    if [ $(warp_check_is_running) = false ]; then
-        warp_message_error "The containers are not running"
-        warp_message_error "please, first run warp start"
+    [ -d $PROJECTPATH/.warp/docker/volumes/php-fpm/ ] && sudo chmod -R 775 $PROJECTPATH/.warp/docker/volumes/php-fpm
+    [ -d $PROJECTPATH/.warp/docker/volumes/php-fpm/ ] && sudo chgrp -R 33 $PROJECTPATH/.warp/docker/volumes/php-fpm
 
-        exit 1;
-    fi
-    
+    exit 1;
+}
+
+function fix_elasticsearch()
+{
+    warp_message "* Applying permissions to subdirectories .warp/docker/volumes/elasticsearch $(warp_message_ok [ok])"
+
+    mkdir -p   $PROJECTPATH/.warp/docker/volumes/elasticsearch
+    sudo chmod -R a+rwx $PROJECTPATH/.warp/docker/volumes/elasticsearch
+
+    exit 1;
+}
+
+function fix_grunt()
+{
+
+    warp_message "* Applying permissions $(warp_message_ok [ok])"
+    sudo find $PROJECTPATH/pub -type d -exec chmod ug+rwx {} \; # Make folders traversable and read/write
+
+    warp_message "* Make files read/write $(warp_message_ok [ok])"
+    sudo find $PROJECTPATH/pub -type f -exec chmod a+rw {} \;  # Make files read/write
+
+    warp_message "* Deleting var/cache $(warp_message_ok [ok])"
+    [ -d $PROJECTPATH/var/cache ] && sudo rm -rf $PROJECTPATH/var/cache
+
+    exit 1;
+}
+
+function fix_mysql()
+{
+    warp_message "* Applying user (mysql) and group (mysql) to MySQL container $(warp_message_ok [ok])"
+
+    # add permission MySQL 999 (mysql inside the container)
+    [ -d $PROJECTPATH/.warp/docker/volumes/mysql/ ] && sudo chown -R 999:999 $PROJECTPATH/.warp/docker/volumes/mysql/
+
+    exit 1;
+}
+
+function fix_default()
+{
     # fix user and groups to current project
     warp_message "* Applying user: $(whoami) and group: www-data to files and folders $(warp_message_ok [ok])"
     sudo chown -R $(whoami):33 $(ls)
@@ -59,16 +87,64 @@ function fix_permissions()
 
     warp_message "* Applying permissions to subdirectories .warp/docker/volumes $(warp_message_ok [ok])"
 
-    # add permission MySQL 999 (mysql inside the container)
-    # [ -d $PROJECTPATH/.warp/docker/volumes/mysql/ ] && sudo chown -R 999:999 $PROJECTPATH/.warp/docker/volumes/mysql/
-
     # add permission elasticsearch inside the container    
     [ -d $PROJECTPATH/.warp/docker/volumes/elasticsearch/ ] && sudo chmod -R a+rwx $PROJECTPATH/.warp/docker/volumes/elasticsearch/
+    [ -d $PROJECTPATH/.warp/docker/volumes/php-fpm/ ] && sudo chmod -R 775 $PROJECTPATH/.warp/docker/volumes/php-fpm
 
     warp_message "* Applying permissions to binaries $(warp_message_ok [ok])"
     # restart correct permissions to warp and binaries
     [ -f $PROJECTPATH/warp ] && sudo chmod a+x $PROJECTPATH/warp
     [ -d $PROJECTPATH/bin ] && sudo chmod a+x $PROJECTPATH/bin/*
+
+    warp_message "* Success $(warp_message_ok [ok])"
+    exit 1;
+}
+
+function fix_permissions() 
+{
+
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        fix_help_usage 
+        exit 1
+    fi;
+
+    if [ $(warp_check_is_running) = false ]; then
+        warp_message_error "The containers are not running"
+        warp_message_error "please, first run warp start"
+
+        exit 1;
+    fi
+
+    case "$1" in
+      "--addUser")
+            fix_add_user 
+            exit 1
+      ;;
+      "--php")
+            fix_php
+            exit 1
+      ;;
+      "--elasticsearch")
+            fix_elasticsearch
+            exit 1
+      ;;
+      "--mysql")
+            fix_mysql
+            exit 1
+      ;;
+      "--all")
+            fix_php
+            fix_mysql
+            fix_elasticsearch
+            fix_grunt
+            fix_add_user
+            exit 1
+      ;;
+      *)
+            fix_default
+      ;;      
+    esac
 }
 
 function fix_main()
@@ -81,10 +157,6 @@ function fix_main()
 
         -h | --help)
             fix_help_usage
-        ;;
-
-        --addUser)
-            fix_add_user
         ;;
 
         *)            
--- .warp/bin/fix_help.sh
+++ .warp/bin/fix_help.sh
@@ -10,6 +10,10 @@ function fix_help_usage()
     warp_message ""
     warp_message_info "Options:"
     warp_message_info   " -h, --help         $(warp_message 'display this help message')"
+    warp_message_info   " --php              $(warp_message 'fix permissions with php')"
+    warp_message_info   " --mysql            $(warp_message 'fix permissions with mysql')"
+    warp_message_info   " --grunt            $(warp_message 'fix permissions grunt')"
+    warp_message_info   " --elasticsearch    $(warp_message 'fix permissions with elasticsearch')"
     warp_message_info   " --addUser          $(warp_message 'add user docker and www-data to current user')"
     warp_message ""
 
